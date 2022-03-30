f11d6b76c3aacb7d0389ab8694d7602f2aea1535
Varac <varac@varac.net>
2022-03-30 19:28:59 +0200

    Add custom-infrastructure kustomization

---
+++ basic/install.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+echo "Creating / updating gitRepository custom-flux-example in namespace flux-system"
+flux create source git custom-flux-example \
+  --url=https://open.greenhost.net/stackspin/custom-flux-example.git \
+  --branch=main \
+  --interval=1h
+
+echo "Creating / updating kustomization custom-flux-example in namespace flux-system"
+flux create kustomization custom-flux-example \
+  --source=GitRepository/custom-flux-example \
+  --path="./basic/clusters/production/" \
+  --prune=true \
+  --interval=1h
