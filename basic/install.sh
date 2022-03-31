b6008bceabd30a72ee77812fa1c6838ad195bc53
Varac <varac@varac.net>
2022-03-31 13:31:40 +0200

    Fix gitrepo name

--- basic/install.sh
+++ basic/install.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
 echo "Creating / updating gitRepository custom-flux-example-basic in namespace flux-system"
-flux create source git custom-flux-example-basic \
+flux create source git custom-flux-example \
   --url=https://open.greenhost.net/stackspin/custom-flux-example.git \
   --branch=main \
   --interval=1h
 
 echo "Creating / updating kustomization custom-flux-example-basic in namespace flux-system"
 flux create kustomization custom-flux-example-basic \
-  --source=GitRepository/custom-flux-example-basic \
+  --source=GitRepository/custom-flux-example \
   --path="./basic/clusters/production/" \
   --prune=true \
   --interval=1h
