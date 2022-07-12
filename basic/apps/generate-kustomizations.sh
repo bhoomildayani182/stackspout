cbe8dcb7d542f095c64e6dc56dc4895886343539
xeruf <27jf@pm.me>
2022-07-12 22:11:36 +0100

    apps: fix kubernetes kustomization

--- basic/apps/generate-kustomizations.sh
+++ basic/apps/generate-kustomizations.sh
@@ -5,5 +5,5 @@ do  echo "$dir"
 	{ echo 'apiVersion: kustomize.config.k8s.io/v1beta1
 kind: Kustomization
 resources:'
-	find $dir -type f -not -name "*.bak" -printf "  - %f\n"; } | tee $dir/kustomization.yaml
+	find $dir -type f -not -name "*.bak" -not -name "kustomization.yaml" -printf "  - %f\n"; } | tee $dir/kustomization.yaml
 done
