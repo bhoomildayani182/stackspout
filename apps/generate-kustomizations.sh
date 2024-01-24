0710ff653260d34808ae2f23cb238e69412eb48c
xeruf <27jf@pm.me>
2024-01-24 15:42:23 +0100

    apps: create kustomization for directory

--- apps/generate-kustomizations.sh
+++ apps/generate-kustomizations.sh
@@ -1,9 +1,14 @@
 #!/bin/sh -e
 # Generates kubernetes kustomizations
-find -mindepth 1 -maxdepth 1 -type d | while read dir;
-do  echo "$dir"
+if test $# -gt 0
+then dir=$1
 	{ echo 'apiVersion: kustomize.config.k8s.io/v1beta1
 kind: Kustomization
 resources:'
-	find $dir -type f -not -name "*.bak" -not -name "kustomization.yaml" -printf "  - %f\n"; } | tee $dir/kustomization.yaml
-done
+	find $dir -maxdepth 1 -type f -name "*.yaml" -not -name "kustomization.yaml" -printf "  - %f\n"; } | tee $dir/kustomization.yaml
+else
+	find -mindepth 1 -maxdepth 1 -type d | while read dir
+		do echo "$dir"
+			$0 "$dir"
+		done
+fi
